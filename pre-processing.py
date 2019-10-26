79db438aca6cf18bcd3883568a7a6ded3cef8144
brian <bgcapbell@email.wm.edu>
2019-10-26 09:21:20 -0500

    ipynb trying to solve oversampling

--- pre-processing.py
+++ pre-processing.py
@@ -7,6 +7,8 @@ class Preprocess():
     this class oversamples and normalizes the datasets.
     '''
     def __init__ (self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
 
 
 # References
