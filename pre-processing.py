9fee3fb8d2966a50357c7b752f6de1117607afc9
brian <bgcapbell@email.wm.edu>
2019-10-28 10:42:59 -0500

    ipynb:updated paths/.py: began oversample function

--- pre-processing.py
+++ pre-processing.py
@@ -1,14 +1,8 @@
 import numpy as np
 import pandas as pd 
 
-class Preprocess():
-    '''
-    After the data has been separated into training and testing data,
-    this class oversamples and normalizes the datasets.
-    '''
-    def __init__ (self, x_data, y_data):
-        self.x_data = x_data
-        self.y_data = y_data
+# This imports the algorithm that will resample the training data.
+# This comes from Reference 11 in References.
+from sklearn.utils import resample
 
 
-# References
